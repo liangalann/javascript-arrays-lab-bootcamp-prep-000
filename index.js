8562fb1daa636ea59ed79ff937a4369a51b9dd75
Alan Liang <liangalann@gmail.com>
2018-03-19 17:05:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function kittens() {
+  return kittens
+}
+
+function destructivelyAppendKitten(name) {
+  kittens.push(name)
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
